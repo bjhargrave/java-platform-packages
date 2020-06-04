e3875b1a079bac56328fe7da28941ff5843a44fe
BJ Hargrave <bj@bjhargrave.com>
2020-06-04 19:00:57 -0400

    Remove use of SortedMap/SortedSet
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- src/main/java/io/hargrave/java/packages/CalculateJavaPlatformPackages.java
+++ src/main/java/io/hargrave/java/packages/CalculateJavaPlatformPackages.java
@@ -1,27 +1,25 @@
 package io.hargrave.java.packages;
 
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 import java.lang.module.ModuleDescriptor;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
-import java.util.SortedMap;
-import java.util.SortedSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.stream.Stream;
 
 public class CalculateJavaPlatformPackages {
 	public static void main(String[] args) {
-		SortedMap<String, SortedSet<String>> moduleExports = modulesExports();
+		Map<String, Set<String>> moduleExports = modulesExports();
 		System.out.println(packagesProperty(moduleExports));
 		System.out.println();
 		System.out.println(modulesProperty(moduleExports));
 	}
 
-	public static String packagesProperty(SortedMap<String, SortedSet<String>> moduleExports) {
+	public static String packagesProperty(Map<String, Set<String>> moduleExports) {
 		return moduleExports.values()
 			.stream()
 			.flatMap(Collection<String>::stream)
@@ -30,35 +28,37 @@ public class CalculateJavaPlatformPackages {
 			.collect(joining(",\\\n ", "org.osgi.framework.system.packages = \\\n ", ""));
 	}
 
-	public static String modulesProperty(SortedMap<String, SortedSet<String>> moduleExports) {
+	public static String modulesProperty(Map<String, Set<String>> moduleExports) {
 		return moduleExports.entrySet()
 			.stream()
 			.filter(entry -> !entry.getValue()
 				.isEmpty())
+			.sorted(Entry.comparingByKey())
 			.map(entry -> entry.getValue()
 				.stream()
+				.sorted()
 				.collect(joining(",\\\n   ", " " + entry.getKey() + ";\\\n  exports:List<String>='\\\n   ", "'")))
 			.collect(joining(",\\\n", "jpms.modules = \\\n", ""));
 	}
 
-	public static SortedMap<String, SortedSet<String>> modulesExports() {
-		SortedMap<String, SortedSet<String>> moduleExports = ModuleLayer.boot()
+	public static Map<String, Set<String>> modulesExports() {
+		Map<String, Set<String>> moduleExports = ModuleLayer.boot()
 			.modules()
 			.stream()
 			.map(Module::getDescriptor)
 			.collect(toMap(ModuleDescriptor::name, CalculateJavaPlatformPackages::moduleExports,
-				CalculateJavaPlatformPackages::merger, TreeMap<String, SortedSet<String>>::new));
+				CalculateJavaPlatformPackages::merger));
 		return moduleExports;
 	}
 
-	private static SortedSet<String> moduleExports(ModuleDescriptor descriptor) {
+	private static Set<String> moduleExports(ModuleDescriptor descriptor) {
 		Stream<String> stream = descriptor.isAutomatic() ? descriptor.packages()
 			.stream()
 			: descriptor.exports()
 				.stream()
 				.filter(e -> !e.isQualified())
 				.map(ModuleDescriptor.Exports::source);
-		return stream.collect(toCollection(TreeSet<String>::new));
+		return stream.collect(toSet());
 	}
 
 	private static <E, SET extends Set<E>> SET merger(SET left, SET right) {
--- src/test/java/io/hargrave/java/packages/AppTest.java
+++ src/test/java/io/hargrave/java/packages/AppTest.java
@@ -2,15 +2,15 @@ package io.hargrave.java.packages;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.SortedMap;
-import java.util.SortedSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class AppTest {
 	@Test
 	public void testApp() {
-		SortedMap<String, SortedSet<String>> moduleExports = CalculateJavaPlatformPackages.modulesExports();
+		Map<String, Set<String>> moduleExports = CalculateJavaPlatformPackages.modulesExports();
 		assertThat(moduleExports).isNotEmpty();
 		System.out.println(CalculateJavaPlatformPackages.packagesProperty(moduleExports));
 		System.out.println();
