ab451a6f8d09f2db073bbc29735fa8f49d4a5098
BJ Hargrave <bj@bjhargrave.com>
2020-06-04 18:32:22 -0400

    Update to emit the contents of a Bnd EE properties file
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

--- src/main/java/io/hargrave/java/packages/CalculateJavaPlatformPackages.java
+++ src/main/java/io/hargrave/java/packages/CalculateJavaPlatformPackages.java
@@ -1,31 +1,69 @@
 package io.hargrave.java.packages;
 
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toMap;
 
 import java.lang.module.ModuleDescriptor;
+import java.util.Collection;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.stream.Stream;
 
 public class CalculateJavaPlatformPackages {
 	public static void main(String[] args) {
-		System.out.println(packagesProperty(calculateVMPackages()));
+		SortedMap<String, SortedSet<String>> moduleExports = modulesExports();
+		System.out.println(packagesProperty(moduleExports));
+		System.out.println();
+		System.out.println(modulesProperty(moduleExports));
 	}
 
-	public static String packagesProperty(Stream<String> packages) {
-		return packages.collect(joining(",\\\n ", "platform.packages = \\\n ", "\n"));
+	public static String packagesProperty(SortedMap<String, SortedSet<String>> moduleExports) {
+		return moduleExports.values()
+			.stream()
+			.flatMap(Collection<String>::stream)
+			.sorted()
+			.distinct()
+			.collect(joining(",\\\n ", "org.osgi.framework.system.packages = \\\n ", ""));
 	}
 
-	public static Stream<String> calculateVMPackages() {
-		return ModuleLayer.boot()
+	public static String modulesProperty(SortedMap<String, SortedSet<String>> moduleExports) {
+		return moduleExports.entrySet()
+			.stream()
+			.filter(entry -> !entry.getValue()
+				.isEmpty())
+			.map(entry -> entry.getValue()
+				.stream()
+				.collect(joining(",\\\n   ", " " + entry.getKey() + ";\\\n  exports:List<String>='\\\n   ", "'")))
+			.collect(joining(",\\\n", "jpms.modules = \\\n", ""));
+	}
+
+	public static SortedMap<String, SortedSet<String>> modulesExports() {
+		SortedMap<String, SortedSet<String>> moduleExports = ModuleLayer.boot()
 			.modules()
 			.stream()
 			.map(Module::getDescriptor)
-			.flatMap(descriptor -> descriptor.isAutomatic() ? descriptor.packages()
-				.stream()
-				: descriptor.exports()
-					.stream()
-					.filter(e -> !e.isQualified())
-					.map(ModuleDescriptor.Exports::source))
-			.sorted()
-			.distinct();
+			.collect(toMap(ModuleDescriptor::name, CalculateJavaPlatformPackages::moduleExports,
+				CalculateJavaPlatformPackages::merger, TreeMap<String, SortedSet<String>>::new));
+		return moduleExports;
 	}
+
+	private static SortedSet<String> moduleExports(ModuleDescriptor descriptor) {
+		Stream<String> stream = descriptor.isAutomatic() ? descriptor.packages()
+			.stream()
+			: descriptor.exports()
+				.stream()
+				.filter(e -> !e.isQualified())
+				.map(ModuleDescriptor.Exports::source);
+		return stream.collect(toCollection(TreeSet<String>::new));
+	}
+
+	private static <E, SET extends Set<E>> SET merger(SET left, SET right) {
+		left.addAll(right);
+		return left;
+	}
+
 }
--- src/test/java/io/hargrave/java/packages/AppTest.java
+++ src/test/java/io/hargrave/java/packages/AppTest.java
@@ -1,18 +1,19 @@
 package io.hargrave.java.packages;
 
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
+import java.util.SortedMap;
+import java.util.SortedSet;
 
 import org.junit.Test;
 
 public class AppTest {
 	@Test
 	public void testApp() {
-		List<String> packages = CalculateJavaPlatformPackages.calculateVMPackages()
-			.collect(toList());
-		assertThat(packages).isNotEmpty();
-		System.out.println(CalculateJavaPlatformPackages.packagesProperty(packages.stream()));
+		SortedMap<String, SortedSet<String>> moduleExports = CalculateJavaPlatformPackages.modulesExports();
+		assertThat(moduleExports).isNotEmpty();
+		System.out.println(CalculateJavaPlatformPackages.packagesProperty(moduleExports));
+		System.out.println();
+		System.out.println(CalculateJavaPlatformPackages.modulesProperty(moduleExports));
 	}
 }
